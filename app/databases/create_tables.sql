ae8ec9772ef17d0754ba36fcaad25116a4eaa8c4
NicoEPITA <root@vps-7a8f04c0.vps.ovh.net>
2023-12-15 17:18:38 +0000

    Change on database for login/register

--- app/databases/create_tables.sql
+++ app/databases/create_tables.sql
@@ -5,7 +5,8 @@ USE HarmoniHub;
 -- Création de la table des utilisateurs
 CREATE TABLE IF NOT EXISTS users (
     id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
+    surname VARCHAR(50) NOT NULL UNIQUE,
+    name VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
