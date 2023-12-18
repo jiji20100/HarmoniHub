f9c035eb0f4652335f2fe7d95ada60abf8ac54d7
SamuelDeliens <samuel.deliens@gmail.com>
2023-12-18 15:33:27 +0000

    change auth page

--- app/Views/Auth/auth.php
+++ app/Views/Auth/auth.php
@@ -6,6 +6,53 @@
     <title>Authentication Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
+
+        body {
+            position: relative;
+            background-color: #000;
+        }
+
+        .image {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-image: url('assets/img/background.png');
+            opacity: 0.9;
+            background-size: cover;
+            background-position: center;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+        }
+
+        .card {
+            position: relative;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            background-color: transparent;
+            border-radius: 1rem;
+            overflow: hidden;
+        }
+
+        .card .background {
+            -webkit-backdrop-filter: blur(15px); /* assure la compatibilité avec safari */
+            backdrop-filter: blur(15px);
+            background-color: rgba(182, 182, 182, 0.1);
+            z-index: 1;
+            position: absolute;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+        }
+
+        .card-body {
+            position: relative;
+            z-index: 2;
+        }
+
+        /* Ajouter un style pour le fond blanc du formulaire */
         .white-bg-form {
             background-color: white;
             padding: 20px;
@@ -15,21 +62,26 @@
     </style>
 </head>
 
-<body style="background-image: url('https://wallpapers.com/images/hd/aesthetic-music-background-cvcbu6do9krpx1wc.jpg'); background-size: cover; background-position: center; margin: 0; padding: 0; overflow: hidden;">
+<body>
+    <div class="image"></div>
+
     <div class="container pl-5 pr-5">
         <div class="row d-flex justify-content-center align-items-center" style="min-height: 100vh;">
             <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                <div class="card bg-white text-dark" style="border-radius: 1rem;">
+                <div class="card text-light" style="border-radius: 1rem;">
+                    <div class="background"></div>
                     <div class="card-body p-5  text-center">
                         <div>
-                            <h2 class="fw-bold mb-3 text-uppercase">Page de Connexion</h2>
+                            <h2 class="fw-bold mb-4 text-uppercase">Page de Connexion</h2>
 
                             <!-- Formulaire de connexion avec fond blanc -->
-                            <form action="/login" method="post" class="white-bg-form">
-                               <a href="/login"><button class="btn btn-outline-dark btn-lg px-5 mb-3">Se connecter</button></a>
-                               <a href="/register"><button class="btn btn-outline-dark btn-lg px-5 mb-3">S'inscrire</button></a>
-                            </form>
-                            <p class="small mb-5 pb-lg-2"><a class="text-dark" href="/reset_password">Forgot password?</a></p>
+                            <div class="col-12 mb-1">
+                                <a href="/login"><button class="btn btn-outline-light btn-lg px-5 mb-3" style="width:70%">Se connecter</button></a>
+                            </div>
+                            <div class="col-12">
+                                <a href="/register"><button class="btn btn-outline-light btn-lg px-5 mb-3" style="width:70%">S'inscrire</button></a>
+                            </div>
+                            <p class="small"><a class="text-light" href="/reset_password">Forgot password?</a></p>
                         </div>
                     </div>
                 </div>
