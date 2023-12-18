e1cad30ba98665ac1d1d27dcd8ea0aa41f8bef5a
HilbertChrys <75853496+kalash98@users.noreply.github.com>
2023-12-18 19:44:14 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -16,6 +16,10 @@ $app = new App($router, $_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
 
 $app->initRoutes();
 
+function isAjaxRequest() {
+    return !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';
+}
+
 ?>
 
 <html lang="en">
@@ -25,5 +29,13 @@ $app->initRoutes();
         <title>App</title>
         <link rel="stylesheet" href="app.css">
     </head>
-    <?php $app->run(); ?>
+    <?php
+    $excludeNavbarOn = ['/login', '/register', '/reset_password', '/'];
+    $currentPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    if (!isAjaxRequest() && !in_array($currentPath, $excludeNavbarOn)) 
+    {
+        include '../app/Resources/layout/navbar.php'; 
+    }
+    $app->run(); 
+    ?>
 </html>
