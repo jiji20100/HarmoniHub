3955af3f35e1c3cde34624c8d8189b04a6b1efc3
SamuelDeliens <samuel.deliens@gmail.com>
2023-12-18 15:21:41 +0000

    correct error with register and login (add username management)

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -2,7 +2,7 @@
 namespace Controllers;
 
 use Source\Renderer;
-use Source\Database;
+use Models\User;
 
 class AuthController {
     public function index(): Renderer {
@@ -22,11 +22,6 @@ class AuthController {
     }
 
     public function login_process(): Renderer {
-        // Rediriger si déjà connecté
-        if (isset($_SESSION['is_logged_in']) && $_SESSION['is_logged_in']) {
-            header('Location: /home');
-            exit;
-        }
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $erreurs = [];
             $email = $_POST['email'];
@@ -38,17 +33,10 @@ class AuthController {
             }
 
             if (empty($erreurs)) {
-                $connexion = Database::getConnection();
-                // Vérifie si l'email existe déjà
-                $query = "SELECT * FROM users WHERE email = :email";
-                $stmt = $connexion->prepare($query);
-                $stmt->bindParam(':email', $email, \PDO::PARAM_STR);
-                $stmt->execute();
-                $user = $stmt->fetch(\PDO::FETCH_ASSOC);
 
+                $user = User::getUserByEmail($email);
                 if ($user) {
                     if (password_verify($password, $user['password'])) {
-                        session_start();
                         $_SESSION['is_logged_in'] = true;
                         $_SESSION['user_id'] = $user['id'];
                         header('Location: /home');
@@ -63,12 +51,7 @@ class AuthController {
 
             if (!empty($erreurs)) {
                 $_SESSION['error'] = implode(' ', $erreurs);
-                header('Location: /login');
-                exit();
             }
-
-            header('Location: /login');
-            exit();
         }
 
         // Si la méthode HTTP n'est pas POST, redirigez vers la page de connexion
@@ -77,16 +60,10 @@ class AuthController {
     }
 
     public function register_process() {
-
-        // Rediriger si déjà connecté
-        if (isset($_SESSION['is_logged_in']) && $_SESSION['is_logged_in']) {
-            header('Location: home.php');
-            exit;
-        }
-
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $surname = $_POST['surname'];
             $name = $_POST['name'];
+            $username = $_POST['username'];
             $email = $_POST['email'];
             $password = $_POST['password'];
             $passwordConfirmation = $_POST['password_confirmation'];
@@ -107,38 +84,42 @@ class AuthController {
             }
 
             if (empty($erreurs)) {
-                try {
-                    $connexion = Database::getConnection();
 
-                    // Vérifier si l'email est déjà utilisé
-                    $stmt = $connexion->prepare("SELECT * FROM users WHERE email = :email");
-                    $stmt->bindParam(':email', $email);
-                    $stmt->execute();
-
-                    if ($stmt->fetch()) {
-                        $erreurs[] = "Cette adresse e-mail est déjà utilisée.";
-                    } else {
-                        // Insérer l'utilisateur dans la base de données
-                        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-                        $stmt = $connexion->prepare("INSERT INTO users (name, surname, email, password) VALUES (:name, :surname, :email, :password)");
-                        $stmt->bindParam(':name', $name);
-                        $stmt->bindParam(':surname', $surname);
-                        $stmt->bindParam(':email', $email);
-                        $stmt->bindParam(':password', $hashedPassword);
-                        $stmt->execute();
-
-                        // Redirection ou gestion de la session
-                        $_SESSION['success'] = "Bravo, vous êtes maintenant inscris !";
-                        $_SESSION['user_id'] = $connexion->lastInsertId();
-                        header('Location: login');
-                        exit;
+                if (User::getUserByEmail($email)) {
+                    $erreurs[] = "Un utilisateur existe déjà avec cet e-mail.";
+                } else {
+                    if (!$username) {
+                        $username = $name . $surname;
                     }
-                } catch (PDOException $e) {
-                    $erreurs[] = "Erreur lors de l'inscription : " . $e->getMessage();
+                    if (User::getUserByArtistName($username)) {
+                        $username = $username . time();
+                    }
+
+                    $user = User::createUser([
+                        'name' => $name,
+                        'surname' => $surname,
+                        'artist_name' => $username,
+                        'email' => $email,
+                        'password' => password_hash($password, PASSWORD_DEFAULT)
+                    ]);
+
+                    if (!$user) {
+                        $erreurs[] = "Erreur lors de l'inscription.";
+                    } else {
+                        $_SESSION['success'] = "Bravo, vous êtes maintenant inscris !";
+                        $_SESSION['user_id'] = $user['id'];
+                        unset($_SESSION['errors']);
+                        header('Location: /login');
+                        exit();
+                    }
                 }
             }
 
-            // Gérer l'affichage des erreurs ici si nécessaire
+            if (!empty($erreurs)) {
+                $_SESSION['errors'] = implode(' ', $erreurs);
+                header('Location: /register');
+                exit();
+            }            
         }
     }
 
--- app/Views/Auth/login.php
+++ app/Views/Auth/login.php
@@ -6,6 +6,52 @@
     <title>Connexion</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
+
+        body {
+            position: relative;
+            background-color: #000;
+        }
+
+        .image {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-image: url('assets/img/background.png');
+            opacity: 0.9;
+            background-size: cover;
+            background-position: center;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+        }
+
+        .card {
+            position: relative;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            background-color: transparent;
+            border-radius: 1rem;
+            overflow: hidden;
+        }
+
+        .card .background {
+            -webkit-backdrop-filter: blur(15px); /* assure la compatibilité avec safari */
+            backdrop-filter: blur(15px);
+            background-color: rgba(182, 182, 182, 0.1);
+            z-index: 1;
+            position: absolute;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+        }
+
+        .card-body {
+            position: relative;
+            z-index: 2;
+        }
+
         /* Ajouter un style pour le fond blanc du formulaire */
         .white-bg-form {
             background-color: white;
@@ -16,52 +62,46 @@
     </style>
 </head>
 
-<body style="background-image: url('https://wallpapers.com/images/hd/aesthetic-music-background-cvcbu6do9krpx1wc.jpg'); background-size: cover; background-position: center; margin: 0; padding: 0; overflow: hidden;">
+<body>
+
+    <div class="image"></div>
 
     <div class="container pl-5 pr-5">
-        <!-- Vérifier et afficher les messages d'erreur ou de succès stockés en session -->
-        <?php if (isset($_SESSION['error'])) : ?>
-            <div class="alert alert-danger">
-                <?= $_SESSION['error']; ?>
-                <?php unset($_SESSION['error']); ?>
-            </div>
-        <?php endif; ?>
-
-
-        <?php if (isset($_SESSION['success'])) : ?>
-            <div class="alert alert-success">
-                <?= $_SESSION['success']; ?>
-                <?php unset($_SESSION['success']); ?>
-            </div>
-        <?php endif; ?>
-
         <div class="row d-flex justify-content-center align-items-center" style="min-height: 100vh;">
             <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                <div class="card bg-white text-dark" style="border-radius: 1rem;">
+                <!-- Vérifier et afficher les messages d'erreur ou de succès stockés en session -->
+                <?php if (isset($_SESSION['error'])) : ?>
+                    <div class="alert alert-danger">
+                        <?= $_SESSION['error']; ?>
+                        <?php unset($_SESSION['error']); ?>
+                    </div>
+                <?php endif; ?>
+                <div class="card text-light">
+                    <div class="background"></div>
                     <div class="card-body p-5  text-center">
                         <div>
                             <h2 class="fw-bold mb-3 text-uppercase">Login</h2>
 
                             <!-- Formulaire de connexion avec fond blanc -->
-                            <form action="/login" method="post" class="white-bg-form">
-                                <div class="form-outline form-dark mb-4">
+                            <form action="/login" method="post" style="padding:10px">
+                                <div class="form-outline form-light mb-4">
                                     <label for="email" class="form-label">Nom d'utilisateur</label>
                                     <input type="text" name="email" id="email" class="form-control form-control-lg" required>
                                 </div>
 
-                                <div class="form-outline form-dark mb-4">
+                                <div class="form-outline form-light mb-4">
                                     <label for="password" class="form-label">Mot de passe</label>
                                     <input type="password" name="password" id="password" class="form-control form-control-lg" required>
                                 </div>
 
-                                <button class="btn btn-outline-dark btn-lg px-5" type="submit">Login</button>
+                                <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
                             </form>
 
-                            <p class="small mb-5 pb-lg-2"><a class="text-dark" href="/reset_password">Forgot password?</a></p>
+                            <p class="small mb-5 pb-lg-2"><a class="text-light" href="/reset_password">Forgot password?</a></p>
                         </div>
 
                         <div>
-                            <p class="mb-0">Don't have an account? <a href="/register" class="text-dark fw-bold">Sign Up</a></p>
+                            <p class="mb-0">Don't have an account? <a href="/register" class="text-light fw-bold">Sign Up</a></p>
                         </div>
                     </div>
                 </div>
