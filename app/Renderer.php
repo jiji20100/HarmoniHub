e1c694c14ab97f0412ce062c444bbe60a12cd0af
HilbertChrys <75853496+kalash98@users.noreply.github.com>
2023-12-18 19:16:21 +0100

    Update Renderer.php

--- app/Renderer.php
+++ app/Renderer.php
@@ -4,19 +4,22 @@ namespace Source;
 
 class Renderer
 {
-    public function __construct(private string $viewsPath) 
-    {}
+    public function __construct(private string $viewsPath, array $data = []) 
+    {
+        $this->data = $data;
+    }
 
     public function view() 
     {
+        extract($this->data);
         ob_start();
         require VIEWS . $this->viewsPath . ".php";
         return ob_get_clean();
     }
 
-    public static function make(string $viewsPath) 
+    public static function make(string $viewsPath, array $data = []): Renderer 
     {
-        return new static($viewsPath);
+        return new static($viewsPath, $data);
     }
 
     public function __toString() 
