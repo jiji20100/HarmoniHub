0f8a85622133c19ae48583802c887b425a1cd8f8
HilbertChrys <75853496+kalash98@users.noreply.github.com>
2023-12-18 19:38:20 +0100

    Update navbar.php

--- app/Resources/layout/navbar.php
+++ app/Resources/layout/navbar.php
@@ -1,17 +1,38 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html>
 <head>
     <style>
         body {
             margin: 0; /* Réinitialise la marge du corps du document */
         }
         .navbar {
+            position: relative;
+            margin: 0;
+            background-image: url('assets/img/background.png');
+        }
+
+        .navbar-background {
+            -webkit-backdrop-filter: blur(15px); /* assure la compatibilité avec safari */
+            backdrop-filter: blur(15px);
+            background-color: rgba(100, 100, 100, 0.2);
+            z-index: 1;
+            position: absolute;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+        }
+
+        .navbar-content {
+            position: relative;
             display: flex;
+            width: calc(100% - 80px);
             justify-content: space-between;
             align-items: center;
-            background-color: #333;
-            color: white;
+            color: black;
             padding: 10px;
+            z-index: 2;
+            margin: 0 40px;
         }
 
         .app-name a {
@@ -20,59 +41,99 @@
             font-size: 20px; 
             font-weight: bold;
             padding: 5px 10px;
+            transition: 0.1s;
+        }
+
+        .app-name a:hover {
+            color: #f44336;
         }
 
         .nav-links {
             list-style: none;
             display: flex;
             justify-content: flex-end; 
-            flex-grow: 1; 
+            padding: 0;
+            gap: 10px;
+        }
+
+        .nav-links li {
+            position: relative;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-width: 120px; 
+            text-align: center; 
         }
 
         .nav-links li a {
             color: white;
             text-decoration: none;
-            margin: 0 20px; 
             font-size: 18px; 
-            padding: 10px 15px; 
         }
 
-        .nav-links li {
-            min-width: 120px; 
-            text-align: center; 
+        .nav-links li a::after {
+            position: absolute;
+            content: '';
+            width: 0;
+            height: 2px;
+            background-color: #f44336;
+            display: block;
+            margin: auto;
+            margin-top: 15px;
+            left: 0;
+            transition: 0.5s;
         }
 
-        @media (max-width: 768px) {
-            .nav-links {
-                display: none;
-                flex-direction: column;
-                width: 100%;
-                text-align: center;
-            }
+        .nav-links li a:hover {
+            color: #f44336;
+        }
 
-            .nav-links.active {
-                display: flex;
-            }
-            .nav-links li a {
-                padding: 15px 20px; /* Augmente le padding pour les appareils mobiles */
-            }
+        .nav-links li a:hover::after {
+            width: 100%;
+        }
+
+        .nav-links li a.active {
+            color: #f44336;
+        }
+
+        .nav-links li a.active::after {
+            width: 100%;
+        }
+
+        .logout-button {
+            background-color: transparent;
+            color: white;
+            border: 1px solid white;
+            border-radius: 5px;
+            padding: 5px 10px;
+            font-size: 16px;
+            font-weight: bold;
+            cursor: pointer;
+            transition: 0.2s;
+        }
+
+        .logout-button:hover {
+            background-color: #f44336;
+            border: 1px solid #f44336;
+            color: white;
+        }
     </style>
 </head>
 <body>
-    <nav class="navbar">
-        <div class="app-name">
-            <a href="/home">HarmoniHub</a>
+    <nav class="navbar" style="padding:0">
+        <div class="navbar-background"></div>
+        <div class="navbar-content">
+            <div class="app-name">
+                <a href="/home">HarmoniHub</a>
+            </div>
+            <ul class="nav-links" id="navLinks">
+                <li><a href="/recherche">Rechercher</a></li>
+                <li><a href="/track">Mes Tracks</a></li>
+                <li><a href="/favorite">Mes Favoris</a></li>
+                <li><a href="/profile">Mon Profil</a></li>
+                <li><button class="logout-button" onclick="window.location.href='/logout'">Se déconnecter</button></li>
+            </ul>
         </div>
-        <ul class="nav-links" id="navLinks">
-            <li><a href="recherche.php">Rechercher</a></li>
-            <li><a href="tracks.php">Mes Tracks</a></li>
-            <li><a href="favorite.php">Mes Favoris</a></li>
-            <li><a href="profile.php">Mon Profil</a></li>
-            <li><form method="POST" action="logout.php">
-                <button type="submit" class="logout-button">Déconnexion</button>
-            </form></li>
-        </ul>
     </nav>
-   
 </body>
 </html>
